ebc77c6291327e30c91efc7ac01e7a9ba75c1729
Fan Yang <yangfanhewangzheng@gmail.com>
2023-09-15 15:01:16 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/greeting', function(req, res){
 app.get('/greeting/:name', (req,res)=>{
     res.send('Hello, '+ req.params.name + ',' + ' It is so great to see you!')
 })
+
+app.get('/tip/:tip/:tipPercentage', (req, res)=>{
+    res.send(req.params.tipPercentage)
+})
+
+
 app.listen(3001, function(){
     console.log('Listening on port 3001')
 })
+
+
